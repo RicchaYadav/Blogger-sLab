00fc3030600d678b76975b8e13dd85ce4ff81aad
Richa <yadavric894@gmail.com>
2024-10-11 14:43:13 +0530

    install react js and tailwind css and create the first template

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+  return (
+    <>
+    <h1> Heyy! Welcome to Bloggers Lab</h1>
+    <h5> A place where you can explore the world of creativity</h5>
+    </>
+  )
+}
+
+export default App
